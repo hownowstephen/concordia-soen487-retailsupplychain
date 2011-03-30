e55b39f2cb5322b65c3f36817a015987cc0017b5
Jose Luis Saldivar <neo.jls@gmail.com>
2011-03-30 11:00:37 -0400

    added Retail service in the RetailWebService project and made the connection to the WarehouseService service

---
+++ RetailWebService/build/generated/jax-wsCache/WarehouseService/org/soen487/supplychain/warehouse/ShipGoods.java
@@ -0,0 +1,87 @@
+
+package org.soen487.supplychain.warehouse;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for shipGoods complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="shipGoods">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="itemList" type="{http://warehouse.supplychain.soen487.org/}itemList" minOccurs="0"/>
+ *         &lt;element name="info" type="{http://warehouse.supplychain.soen487.org/}customer" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "shipGoods", propOrder = {
+    "itemList",
+    "info"
+})
+public class ShipGoods {
+
+    protected ItemList itemList;
+    protected Customer info;
+
+    /**
+     * Gets the value of the itemList property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ItemList }
+     *     
+     */
+    public ItemList getItemList() {
+        return itemList;
+    }
+
+    /**
+     * Sets the value of the itemList property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ItemList }
+     *     
+     */
+    public void setItemList(ItemList value) {
+        this.itemList = value;
+    }
+
+    /**
+     * Gets the value of the info property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Customer }
+     *     
+     */
+    public Customer getInfo() {
+        return info;
+    }
+
+    /**
+     * Sets the value of the info property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Customer }
+     *     
+     */
+    public void setInfo(Customer value) {
+        this.info = value;
+    }
+
+}
---
+++ RetailWebService/src/java/org/soen487/supplychain/retail/Retail.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.soen487.supplychain.retail;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceRef;
+import org.soen487.supplychain.warehouse.ItemShippingStatusList;
+import org.soen487.supplychain.warehouse.WarehouseService;
+
+/**
+ *
+ * @author jose
+ */
+@WebService()
+public class Retail {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/WarehouseService/WarehouseService.wsdl")
+    private WarehouseService service;
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "Retail")
+    public String Retail(@WebParam(name = "itemList")
+    org.soen487.supplychain.warehouse.ItemList itemList, @WebParam(name = "custInfo")
+    org.soen487.supplychain.warehouse.Customer custInfo) {
+        
+        return null;
+    }
+
+    private ItemShippingStatusList shipGoods(org.soen487.supplychain.warehouse.ItemList itemList, org.soen487.supplychain.warehouse.Customer info) {
+        org.soen487.supplychain.warehouse.Warehouse port = service.getWarehousePort();
+        return port.shipGoods(itemList, info);
+    }
+
+}
