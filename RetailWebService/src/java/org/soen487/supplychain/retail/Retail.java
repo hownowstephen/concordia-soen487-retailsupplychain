2d4bed9c19e054f1a45c626e36f0510c59a82fae
Jose Luis Saldivar <neo.jls@gmail.com>
2011-04-02 17:42:45 -0400

    moved submitOrder code to its own java file for easier merging

--- RetailWebService/src/java/org/soen487/supplychain/retail/Retail.java
+++ RetailWebService/src/java/org/soen487/supplychain/retail/Retail.java
@@ -21,56 +21,18 @@ import org.soen487.supplychain.warehouse.*;
 @WebService()
 public class Retail {
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/WarehouseService/WarehouseService.wsdl")
-    private WarehouseService service;
-    private List<Integer> warehouse_indexes;
-    private ItemShippingStatusList list_stat;
-   
+
 
     /**
      * Web service operation
      */
     @WebMethod(operationName = "Retail")
-    public String Retail(@WebParam(name = "itemList")
+    public ItemShippingStatusList submitOrder(@WebParam(name = "itemList")
     ItemList itemList, @WebParam(name = "custInfo")
     Customer custInfo) {
-        warehouse_indexes = Arrays.asList(1,2,3);
-        Collections.shuffle(warehouse_indexes); // random order of warehouse calls
+        submitOrder order = new submitOrder(itemList, custInfo);
 
-        list_stat = warehouse_call(itemList, custInfo, 1);
-
-        list_stat.getItems();
-
-//        for(Integer rand : warehouse_indexes){
-//            list_stat = warehouse_call(itemList, custInfo, rand.intValue());
-//
-//            //list_stat.
-//        }
-        
-
-        return null;
-    }
-
-    private ItemShippingStatusList warehouse_call(ItemList itemList, Customer custInfo, int rand){
-        switch(rand){
-            case 1:
-                return shipGoods_W1(itemList, custInfo);
-            case 2:
-                return shipGoods_W1(itemList, custInfo);
-            case 3:
-                return shipGoods_W1(itemList, custInfo);
-            default:
-                return null;
-        }
-    }
-
-
-    private void updateList(){
-
-    }
-
-    private ItemShippingStatusList shipGoods_W1(ItemList itemList, Customer info) {
-        Warehouse port = service.getWarehousePort();
-        return port.shipGoods(itemList, info);
+        return order.execute();
     }
 
 }
---
+++ RetailWebService/src/java/org/soen487/supplychain/retail/submitOrder.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.soen487.supplychain.retail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import org.soen487.supplychain.warehouse.*;
+
+/**
+ *
+ * @author Jose
+ */
+public class submitOrder {
+
+    private ArrayList<Integer> warehouse_indexes;
+    private ItemShippingStatusList list_stat;
+    private ItemList itemList;
+    private Customer custInfo;
+
+    public submitOrder() {
+    }
+
+    submitOrder(ItemList itemList, Customer custInfo) {
+        this.itemList = itemList;
+        this.custInfo = custInfo;
+
+    }
+
+    public ItemShippingStatusList execute() {
+        warehouse_indexes = (ArrayList<Integer>) Arrays.asList(1, 2, 3);
+        Collections.shuffle(warehouse_indexes); // sets a random ordered list of warehouse to be called
+
+        list_stat = warehouse_call(itemList, custInfo, 1); // TEST PURPOSES ONLY - remove eventually
+
+        list_stat.getItems();
+
+        // call the warehouses in the random ordered list
+//        for(Integer rand : warehouse_indexes){
+//            list_stat = warehouse_call(itemList, custInfo, rand.intValue());
+//
+//            //list_stat.
+//        }
+        return null;
+    }
+
+    private ItemShippingStatusList warehouse_call(ItemList itemList, Customer custInfo, int rand) {
+        switch (rand) {
+            case 1: // Warehouse 1
+                return shipGoods(itemList, custInfo);
+            case 2: // Warehouse 2
+                return shipGoods(itemList, custInfo);
+            case 3: // Warehouse 3
+                return shipGoods(itemList, custInfo);
+            default:
+                return null;
+        }
+    }
+
+    private void updateList() {
+    }
+
+    private static ItemShippingStatusList shipGoods(org.soen487.supplychain.warehouse.ItemList itemList, org.soen487.supplychain.warehouse.Customer info) {
+        org.soen487.supplychain.warehouse.WarehouseService service = new org.soen487.supplychain.warehouse.WarehouseService();
+        org.soen487.supplychain.warehouse.Warehouse port = service.getWarehousePort();
+        return port.shipGoods(itemList, info);
+    }
+
+}
