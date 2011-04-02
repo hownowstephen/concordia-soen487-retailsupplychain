f10f25dccb2a6a265eb5e7dbbda7e148c74a0117
Jose Luis Saldivar <neo.jls@gmail.com>
2011-04-02 12:44:53 -0400

    fixed the references and import statements

--- RetailWebService/src/java/org/soen487/supplychain/retail/Retail.java
+++ RetailWebService/src/java/org/soen487/supplychain/retail/Retail.java
@@ -6,17 +6,13 @@
 package org.soen487.supplychain.retail;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.xml.ws.WebServiceRef;
-import org.soen487.supplychain.warehouse.Customer;
-import org.soen487.supplychain.warehouse.ItemList;
-import org.soen487.supplychain.warehouse.ItemShippingStatusList;
-import org.soen487.supplychain.warehouse.WarehouseService;
+import org.soen487.supplychain.warehouse.*;
 
 /**
  *
@@ -35,9 +31,8 @@ public class Retail {
      */
     @WebMethod(operationName = "Retail")
     public String Retail(@WebParam(name = "itemList")
-    org.soen487.supplychain.warehouse.ItemList itemList, @WebParam(name = "custInfo")
-    org.soen487.supplychain.warehouse.Customer custInfo) {
-
+    ItemList itemList, @WebParam(name = "custInfo")
+    Customer custInfo) {
         warehouse_indexes = Arrays.asList(1,2,3);
         Collections.shuffle(warehouse_indexes); // random order of warehouse calls
 
@@ -73,8 +68,8 @@ public class Retail {
 
     }
 
-    private ItemShippingStatusList shipGoods_W1(org.soen487.supplychain.warehouse.ItemList itemList, org.soen487.supplychain.warehouse.Customer info) {
-        org.soen487.supplychain.warehouse.Warehouse port = service.getWarehousePort();
+    private ItemShippingStatusList shipGoods_W1(ItemList itemList, Customer info) {
+        Warehouse port = service.getWarehousePort();
         return port.shipGoods(itemList, info);
     }
 
