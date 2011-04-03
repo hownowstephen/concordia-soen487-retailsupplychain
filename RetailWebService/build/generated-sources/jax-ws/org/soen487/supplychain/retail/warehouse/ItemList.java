ea9ebe86c73616ea87b7cc27fea95fb6f45d5a12
Stephen Young <stephen@tryllo.com>
2011-04-02 20:05:04 -0400

    added ignores

--- RetailWebService/build/generated-sources/jax-ws/org/soen487/supplychain/retail/warehouse/ItemList.java
+++ RetailWebService/build/generated-sources/jax-ws/org/soen487/supplychain/retail/warehouse/ItemList.java
@@ -1,74 +1,3 @@
-<<<<<<< HEAD:RetailWebService/build/generated-sources/jax-ws/org/soen487/supplychain/warehouse/ItemList.java
-
-package org.soen487.supplychain.warehouse;
-
-import java.util.ArrayList;
-import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
-
-
-/**
- * <p>Java class for itemList complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="itemList">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="items" type="{http://warehouse.supplychain.soen487.org/}item" maxOccurs="unbounded" minOccurs="0"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "itemList", propOrder = {
-    "items"
-})
-public class ItemList {
-
-    @XmlElement(nillable = true)
-    protected List<Item> items;
-
-    /**
-     * Gets the value of the items property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the items property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getItems().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link Item }
-     * 
-     * 
-     */
-    public List<Item> getItems() {
-        if (items == null) {
-            items = new ArrayList<Item>();
-        }
-        return this.items;
-    }
-
-}
-=======
 
 package org.soen487.supplychain.retail.warehouse;
 
@@ -138,4 +67,3 @@ public class ItemList {
     }
 
 }
->>>>>>> Jose:RetailWebService/build/generated-sources/jax-ws/org/soen487/supplychain/retail/warehouse/ItemList.java
--- RetailWebService/build/generated/jax-wsCache/WarehouseService/org/soen487/supplychain/retail/warehouse/package-info.java
+++ RetailWebService/build/generated/jax-wsCache/WarehouseService/org/soen487/supplychain/retail/warehouse/package-info.java
@@ -1,7 +1,2 @@
-<<<<<<< HEAD:RetailWebService/build/generated/jax-wsCache/WarehouseService/org/soen487/supplychain/warehouse/package-info.java
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://warehouse.supplychain.soen487.org/")
-package org.soen487.supplychain.warehouse;
-=======
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://warehouse.supplychain.soen487.org/")
 package org.soen487.supplychain.retail.warehouse;
->>>>>>> Jose:RetailWebService/build/generated/jax-wsCache/WarehouseService/org/soen487/supplychain/retail/warehouse/package-info.java
